a7e6b45d7e292b44bcdb3ee335d604660f9c1373
pawan suthar <35306329+pawan187@users.noreply.github.com>
2021-07-15 09:40:47 +0530

    userDetails

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,49 @@
 var express = require("express");
 var router = express.Router();
+var mongoose = require("mongoose");
+
+var db = require("../routes/database");
+// create an schema
+var userSchema = new mongoose.Schema(
+  {
+    full_name: String,
+    email: String,
+    password: String
+  },
+  {
+    collection: "users"
+  }
+);
+var model = mongoose.model("model", userSchema);
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  model.find({}, function (err, result) {
+    if (err) throw err;
+    if (result) {
+      console.log(result);
+      res.json(result);
+    } else {
+      res.send(
+        JSON.stringify({
+          error: "Error"
+        })
+      );
+    }
+  });
 });
-router.get("/about", (req, res) => {
-  console.log("about rqurest");
-  res.send("Hello");
+router.post("/login", (req, res) => {
+  var name = req.body.name;
+  var email = req.body.username;
+  var password = req.body.password;
+  var newUser = new model({
+    email: email,
+    password: password,
+    full_name: name
+  });
+  newUser.save(function (err, user) {
+    if (err) return console.error(err);
+    console.log(user.full_name + " saved to bookstore collection.");
+  });
 });
 module.exports = router;
